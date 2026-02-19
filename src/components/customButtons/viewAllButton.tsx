0590313e2e189efcc3338558e6ba4d7d387a7e3d
Arijit Khan <arijit.khan@broadifitech.com>
2026-02-19 13:47:09 +0530

    feat: update view al l custom button

--- src/components/customButtons/viewAllButton.tsx
+++ src/components/customButtons/viewAllButton.tsx
@@ -7,18 +7,16 @@ interface ButtonProps {
 
 function ViewAllButton({ children }: ButtonProps) {
   return (
-    <div className="flex justify-end">
-      <button
-        className="flex gap-2 justify-center items-center px-3 sm:px-5 py-2 rounded-lg bg-linear-to-r from-button-blue to-button-sky
+    <button
+      className="flex gap-2 justify-center items-center px-3 sm:px-5 py-2 rounded-lg bg-linear-to-r from-button-blue to-button-sky
                    text-base text-white font-semibold
                    hover:from-blue-600 hover:to-blue-600 hover:shadow-md
                    transition-colors duration-200
                    shadow-sm cursor-pointer"
-      >
-        <p className="text-sm sm:text-base">{children}</p>
-         <MoveRight className='w-4 sm:w-5 ' />
-      </button>
-    </div>
+    >
+      <p className="text-sm sm:text-base">{children}</p>
+      <MoveRight className="w-4 sm:w-5 " />
+    </button>
   );
 }
 
