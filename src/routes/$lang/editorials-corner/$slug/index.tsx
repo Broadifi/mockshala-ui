58e29a0791ec5b28dba3c9b05857fc50c18be200
Souvik-code-ai <purkaitsouvik531@gmial.com>
2026-02-24 15:47:18 +0530

    new edit in editorial corner route

--- src/routes/$lang/editorials-corner/$slug/index.tsx
+++ src/routes/$lang/editorials-corner/$slug/index.tsx
@@ -1,4 +1,4 @@
-import ComingSoon from "@/modules/comingSoon";
+// import ComingSoon from "@/modules/comingSoon";
 import { createFileRoute } from "@tanstack/react-router";
 import EditorialCornerDetails from "@/modules/editorialCorner/editorialCornerDetails";
 export const Route = createFileRoute("/$lang/editorials-corner/$slug/")({
--- src/routes/$lang/exams/index.tsx
+++ src/routes/$lang/exams/index.tsx
@@ -1,18 +1,11 @@
-<<<<<<< Updated upstream
 import ComingSoon from "@/modules/comingSoon";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/$lang/exams/")({
-=======
-import { createFileRoute } from '@tanstack/react-router'
-import ExamsModule from '@/modules/exams'
-export const Route = createFileRoute('/$lang/exams/')({
->>>>>>> Stashed changes
   component: RouteComponent,
 });
 
 function RouteComponent() {
-<<<<<<< Updated upstream
   return (
     <div>
       <p className="text-center">All Exams will appear here</p>
@@ -20,7 +13,4 @@ function RouteComponent() {
       <ComingSoon />
     </div>
   );
-=======
-  return <ExamsModule/>
->>>>>>> Stashed changes
 }
