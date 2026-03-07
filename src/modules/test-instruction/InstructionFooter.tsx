3f63fd73c632e37bbccf96bc9a1f918db0ffb540
Arijit Khan <arijit.khan@broadifitech.com>
2026-03-07 18:08:47 +0530

    feat: added language switch on Instruction footer.

--- src/modules/test-instruction/InstructionFooter.tsx
+++ src/modules/test-instruction/InstructionFooter.tsx
@@ -1,14 +1,35 @@
 import { useState } from 'react';
 
 import { ExamLanguageSelector } from './examInsLangSelector';
+import { useExamLanguage } from '@/stores/examLanguageStore';
+import i18n from '@/i18n';
 
 function InstructionFooter() {
   const [checked, setChecked] = useState(false);
 
+    const { examCurrentLang } = useExamLanguage();
+
+  const getLocalTranslation = (key: string): string => {
+    const bundle = i18n.getResourceBundle(
+      examCurrentLang,
+      "translation",
+    ) as Record<string, unknown>;
+
+    return (
+      (key.split(".").reduce<unknown>((acc, k) => {
+        if (typeof acc === "object" && acc !== null && k in acc) {
+          return (acc as Record<string, unknown>)[k];
+        }
+        return undefined;
+      }, bundle) as string) || key
+    );
+  };
+
+
   return (
     <div className='w-full mx-auto space-y-4 text-xs sm:text-sm xl:text-base'>
       <div className='flex items-center gap-2 md:gap-4 justify-end'>
-        <h4 className='text-blue-900'>Choose your language</h4>
+        <h4 className='text-blue-900'> {getLocalTranslation("examInstructions.chooseLanguage")}</h4>
 
         <ExamLanguageSelector/>
       </div>
@@ -22,8 +43,8 @@ function InstructionFooter() {
           className='min-w-4 min-h-4 accent-blue-500 cursor-pointer border hover:border-blue-500'
         />
 
-        <label htmlFor='instructions '>
-          I have read all the instructions carefully and abide by them
+        <label htmlFor='instructions' className='text-gray-600'>
+           {getLocalTranslation("examInstructions.confirmInstructions")}
         </label>
       </div>
 
@@ -32,13 +53,13 @@ function InstructionFooter() {
           <button 
              onClick={() => window.close()}
             className=' border rounded-md py-1 px-3 sm:px-5 hover:text-red-600 hover:border-red-600 cursor-pointer text-gray-800'>
-            CANCEL
+            {getLocalTranslation("examInstructions.cancel")}
           </button>
           <button
             disabled={!checked}
             className={` ${checked ? 'cursor-pointer bg-linear-to-r from-button-sky to-button-blue text-white' : 'cursor-not-allowed bg-gray-200 text-gray-600'} border rounded-md py-1 px-4`}
           >
-            I AM READY TO BEGIN
+            {getLocalTranslation("examInstructions.readyToBegin")}
           </button>
         </div>
       </div>
