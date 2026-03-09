c6071eb8d7edfc166c6fbbd69e785784031931ef
Arijit Khan <arijit.khan@broadifitech.com>
2026-03-10 00:01:46 +0530

    feat: introduce HomeModule component with content sections and scroll-based login prompt for unauthenticated users.

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import AllTestSeries from "./components/allTestSeries";
 import CurrentAffairs from "./components/currentAffairs";
 import EditorialCornerHome from "./components/EditorialCornerHome";
@@ -6,19 +7,43 @@ import Plans from "./components/plans";
 import PopularExam from "./components/popularExam";
 import Stats from "./components/stats";
 import WhyChoose from "./components/whyChoose";
+import { useAuthStore } from "@/stores/authStore";
+import { useLoginStore } from "@/stores/loginStore";
+
+let hasAutoLoginTriggered = false;
 
 function HomeModule() {
+  const { accessToken } = useAuthStore((state) => state.auth);
+  const { setLoginState } = useLoginStore();
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Trigger modal automatically if not logged in and user scrolls down 300px
+      if (!hasAutoLoginTriggered && !accessToken && window.scrollY > 280) {
+        hasAutoLoginTriggered = true;
+        setLoginState(true);
+      }
+    };
+
+    // Attach listener
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, [accessToken, setLoginState]);
+
   return (
     <div className="w-full mx-auto space-y-12 lg:space-y-15 xl:space-y-20 bg-gray-100/40 ">
       <Hero />
       <CurrentAffairs />
       <AllTestSeries />
-      <PopularExam/> 
-      <EditorialCornerHome/>  
-      <WhyChoose />  
-      <Plans/>   
-      <Stats/>
-      
+      <PopularExam />
+      <EditorialCornerHome />
+      <WhyChoose />
+      <Plans />
+      <Stats />
     </div>
   );
 }
